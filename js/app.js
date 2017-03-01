d01add6198ecbfb4e180cd3179ad52d1023e1bd4
Dexter Lewis <dexterlewis@gmail.com>
2017-03-01 17:48:57 -0500

    Update js and html so resultsl show up in a grid

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function getDataFromYoutube(searchTerm, callback) {
         q: searchTerm,
         part: 'snippet',
         key: apiKey,
-        maxResults: 5,
+        maxResults: 16,
         order: 'date',
 
 
@@ -20,20 +20,46 @@ function getDataFromYoutube(searchTerm, callback) {
 
 function displayResults(data) {
     var resultElement = '';
+    var resultRow = '';
+    debugger;
     $.each(data.items, function(key, item){
+        
+
         var vidTitle = item.snippet.title;
         var vidImage = item.snippet.thumbnails.high.url;
         var vidId = item.id.videoId;
         var vidUrl = 'https://www.youtube.com/watch?v='
-        resultElement +=
-        '<img class="img-responsive" src="'+vidImage+'">' +
-        '<p><a href="' + vidUrl + vidId+'">'+ item.snippet.title + '</a></p></div>';
         
+        resultElement +=
+        '<div class="col-md-3"><img class="img-responsive" src="'+vidImage+'">' +
+        '<p style="height: 50px;"><a href="' + vidUrl + vidId+'">'+ item.snippet.title + '</a></p></div>';
+        if ((key + 1) % 4 === 0) {
+            resultRow += '<div>' + resultElement + '</div>';
+            resultElement = '';
+        }
     });
-        $('.js-results').html(resultElement);
+        $('.js-results').html(resultRow + '<div>' + resultElement + '</div>');
         console.log('video URL' + vidUrl);
     
 }
+
+// function displayInRows(data) {
+//     var resultElement = '';
+    
+//     $.each(data.items, function(key, item){
+//         var vidTitle = item.snippet.title;
+//         var vidImage = item.snippet.thumbnails.high.url;
+//         var vidId = item.id.videoId;
+//         var vidUrl = 'https://www.youtube.com/watch?v='
+//         resultElement +=
+//         '<img class="img-responsive" src="'+vidImage+'">' +
+//         '<p><a href="' + vidUrl + vidId+'">'+ item.snippet.title + '</a></p>';
+        
+//     });
+//         $('.js-results').html(resultElement);
+//         console.log('video URL' + vidUrl);
+    
+// }
 
 //Event listener
 function formWatch() {
